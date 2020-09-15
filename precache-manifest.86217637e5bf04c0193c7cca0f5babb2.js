66526a2a89e1f134e81f968e95f1aac740448ae4
Nihal <nihalnyt@gmail.com>
2020-09-15 10:43:27 +0530

    Updates

---
+++ precache-manifest.86217637e5bf04c0193c7cca0f5babb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8af0d98b994b5749e78f71b922b4297",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "798bff9964cbc7aab43e",
+    "url": "/todo-app/static/css/main.b5a0bd16.chunk.css"
+  },
+  {
+    "revision": "7e09ff1ccbec5eeb8a44",
+    "url": "/todo-app/static/js/2.d161fe4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.d161fe4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798bff9964cbc7aab43e",
+    "url": "/todo-app/static/js/main.a5a2a88c.chunk.js"
+  },
+  {
+    "revision": "febb1defb014fe768eb2",
+    "url": "/todo-app/static/js/runtime-main.8fcd9add.js"
+  }
+]);
