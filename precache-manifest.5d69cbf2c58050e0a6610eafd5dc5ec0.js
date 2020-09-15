575f1fa151eadf6f0268407cc2e8d5f0e6e4f2a1
Nihal <nihalnyt@gmail.com>
2020-09-15 10:38:36 +0530

    Updates

---
+++ precache-manifest.5d69cbf2c58050e0a6610eafd5dc5ec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705cd000eb8fa20e62afccd484c11ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acce16be62be93a05e8b",
+    "url": "/static/css/main.b5a0bd16.chunk.css"
+  },
+  {
+    "revision": "7e09ff1ccbec5eeb8a44",
+    "url": "/static/js/2.d161fe4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d161fe4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acce16be62be93a05e8b",
+    "url": "/static/js/main.52a37f91.chunk.js"
+  },
+  {
+    "revision": "830fb30a987bf42dafa4",
+    "url": "/static/js/runtime-main.84c21d94.js"
+  }
+]);
